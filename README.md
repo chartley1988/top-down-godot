4b1893b1017901e30c555b3e71f62dbd0b035514
Carson Hartley <chartley1988@gmail.com>
2024-07-22 21:30:04 -0600

    add test maps, no real logic yet

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+# Project Structure
+The project folder is divided into several key features.
+
+- `/source`: 
+The source folder contains the main engine for the game. Logic such as movement, battles, inventory live in the source folder.
+
+- `/same`:
+The game folder contains the main content of the project. Files for maps, events, and data are stored here. Most contents of the game folder should be 'instances' of classes, rather than any new definitions.
+
+
 # Godot4-TiledImporter
 Import Tiled Maps into Godot 4
--- main.gd
+++ main.gd
@@ -1,13 +1,19 @@
 extends Node
 
+"""
+
+This scene functions as the first and last script the game runs. It operates on the outside level.
+
+"""
+# Game Variables
+# TODO: As many variables as possible should be moved out of this script eventually
+@export var initial_map: PackedScene
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	printName("Carson")
+	print("Game started successfully!")
 
-
-func printName(name: String):
-	print("Hello " + name)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +21,5 @@ func _process(_delta):
 	pass
 
 
-func _input(event):
-	if not event is InputEventMouseMotion:
-		print(event.as_text())
+
+
